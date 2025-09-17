0f504763cb5f4df49ad04f7afa83d7478a0cf49a
jucagovi <jc.gomez@edu.gva.es>
2025-09-17 17:33:45 +0200

    Configuración GitHubPages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  //base: "./", // O 'tu_subdirectorio/' si se despliega en un subdirectorio.
+  base: "https://jucagovi.github.io/Notas",
+});
